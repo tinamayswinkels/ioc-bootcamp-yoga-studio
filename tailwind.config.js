6a70ddde1170bf3af4f00281efaa65a295bb656d
tinamayswinkels <tinamayswinkels@users.noreply.github.com>
2023-06-18 19:04:14 +0800

    style classes

---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      './_includes/**/*.{html,js,md}',
+      './_layouts/**/*.{html,js,md}',
+      './_*/*.{html,js,md}',
+      './*.{html,js,md}'
+    ],
+    theme: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+        serif: ['Oranienbaum', 'serif'],
+      },
+      colors: {
+        'white': '#ffffff',
+        'black': 'black',
+        'copper': '#C97324',
+        'light': '#FBF2E9', 
+        'blue': 'blue',
+        'purple': 'purple',
+        'teal': 'teal'
+
+      },
+      extend: {
+        spacing: {
+          '5p': '5%',
+          '10p': '10%',
+          '15p': '15%',
+        },
+        aspectRatio: {
+          'landscape': '2 / 1 ',
+          'portrait': '3 / 4',
+          'portrait-tall': '3 / 5',
+        },
+        minHeight:{
+          '100': '100vh',
+          '90': '90vh',
+          '80': '80vh',
+          '70': '70vh',
+          '60': '60vh',
+          '50': '50vh',
+        },
+        screens: {
+            '3xl': '2000px',
+        },
+      },
+    },
+    plugins: [
+      require('@tailwindcss/typography'),
+    ],
+  }
